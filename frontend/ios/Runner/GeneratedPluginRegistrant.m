9d95ac7ba3375b2398430b0a807e0e01d59a58b7
dcgwdgx <dcgwdgx@github.com>
2026-06-13 07:53:08 +0800

    feat: purchases_flutter v5 CocoaPods + SPM-free Podfile + ExportOptions + Gemfile

--- frontend/ios/Runner/GeneratedPluginRegistrant.m
+++ frontend/ios/Runner/GeneratedPluginRegistrant.m
@@ -12,10 +12,17 @@
 @import audioplayers_darwin;
 #endif
 
+#if __has_include(<purchases_flutter/PurchasesFlutterPlugin.h>)
+#import <purchases_flutter/PurchasesFlutterPlugin.h>
+#else
+@import purchases_flutter;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [AudioplayersDarwinPlugin registerWithRegistrar:[registry registrarForPlugin:@"AudioplayersDarwinPlugin"]];
+  [PurchasesFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"PurchasesFlutterPlugin"]];
 }
 
 @end
